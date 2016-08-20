336354deac691c04c09cfd83670fc8b55a38e982
Yusuf Gandhi <gandhi.putra@gmail.com>
2016-08-20 12:41:29 +1000

    Changed the function to just process one channel at a time

--- SCRIPT_002_2016_08_19_trainingAutoEncoderEXP004_25_percent.m
+++ SCRIPT_002_2016_08_19_trainingAutoEncoderEXP004_25_percent.m
@@ -3,38 +3,28 @@
 % use the EXP004C.mat file
 % to run this script properly
 % - END OF INSTRUCTION -
-function [autoEnc_G_10, autoEnc_B_10, tendgreen, tendblue]  = SCRIPT_002_2016_08_19_trainingAutoEncoderEXP004_25_percent(channelG, channelB)
+
+% param channel -> The channel you want to train
+% param channelType -> The string representation of the channel (useful for
+% 
+function [autoEncoderResult, tend]  = SCRIPT_002_2016_08_19_trainingAutoEncoderEXP004_25_percent(channel, channelType)
 
     % running the script 
     hiddenSize_10 = 1920; % 10% of the neurons
 
+    
+    filename = sprintf('EXP005-%s-autoencoder%s_result_25.mat', datetime('now','TimeZone','local','Format', 'yyyy-MM-dd', channelType));
 
-    filename = sprintf('EXP005-%s-autoencoder_result_25.mat', datetime('now','TimeZone','local','Format', 'yyyy-MM-dd'));
-
-    disp('training autoencoder G...');
-    disp('   training 10\%...')
+    disp('training autoencoder');
+    disp('   training 10\%...');
     tstart = tic;
-    autoEnc_G_10 = trainAutoencoder(channelG, hiddenSize_10, 'useGPU', true);
-    tendgreen = toc(tstart);
-    disp('   DONE')
-    disp('training autoencoder G done');
-
+    autoEncoderResult = trainAutoencoder(channel, hiddenSize_10, 'useGPU', true);
+    tend = toc(tstart);
+    disp('   DONE');
+    disp('training autoencoder done');
 
     % saving to the file and clearing the workspace
-    save(filename, 'autoEnc_G_10', 'tendgreen')
-    clearvars -except hiddenSize_10
-
-    disp('training autoencoder B...');
-    disp('   training 10\%...')
-    tstart = tic;
-    autoEnc_B_10 = trainAutoencoder(channelB, hiddenSize_10, 'useGPU', true);
-    tendblue = toc(tstart);
-    disp('   DONE')
-    disp('training autoencoder B done');
-
-
-    % saving to the file and clearing the workspace
-    save(filename, 'autoEnc_B_10','tendblue','-append')
-    clearvars
-    disp('Training autoencoders DONE');
+    disp('Saving %s...', filename);
+    save(filename, 'autoEncoderResult', 'tend');
+    disp('DONE');
 end
